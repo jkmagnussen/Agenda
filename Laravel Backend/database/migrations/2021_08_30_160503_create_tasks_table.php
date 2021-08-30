8070cd15f764d2500cec877f3714e58bbbc884d9
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2021-08-30 22:32:12 +0100

    first committ

---
+++ Laravel Backend/database/migrations/2021_08_30_160503_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('tasks');
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("content");
+            $table->boolean("done");
+            $table->integer("userId");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
