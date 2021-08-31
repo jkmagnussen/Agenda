5ca8b2c4405d236ea006ad16a8a939829f3dc07d
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2021-09-01 00:33:24 +0100

    functional to do app

--- React Front End/src/App.js
+++ React Front End/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
-function App() {
+function App(){
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+
+
+  useEffect(() =>{
+    filterHandler()
+  }, [todos, status]);
+
+
+  const filterHandler = () =>
+  {
+    switch (status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>AGENDA</h1>
+        
       </header>
+            <h3 style={{textAlign:"center", fontSize: "4vh"}}>hello username</h3>
+
+      <Form
+        setStatus={setStatus}
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        />
+      
+      <TodoList todos={todos}
+        inputText={inputText}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}/>
     </div>
-  );
+  ); 
 }
 
 export default App;
